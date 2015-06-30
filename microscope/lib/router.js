0e6a6b6286b09e6e7d5b99f06447aba4e2ad8189
Tony H <tony.hetrick@gmail.com>
2015-06-29 18:33:01 -0600

    Prevent access to submit page and hide button if not logged in

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -18,5 +18,22 @@ Router.route('/posts/:_id', {
 // Page for submitting a new post
 Router.route('/submit', {name: 'postSubmit'});
 
-// 404 not found template.
+// If user is not logged in, show access denied form
+var requireLogin = function() {
+  if (! Meteor.user()) {
+
+    // If actively logging in, show loading Template
+    if (Meteor.loggingIn()) {
+      this.render(this.loadingTemplate);
+    } else {
+      this.render('accessDenied');
+    }
+    
+  } else {
+    this.next();
+  }
+};
+
+// Checks prior processing the page request
 Router.onBeforeAction('dataNotFound', {only: 'postPage'});
+Router.onBeforeAction(requireLogin, {only: 'postSubmit'});

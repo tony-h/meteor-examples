87a86988f8f9f7d6544fdbc77a047cc86e80eccf
Tony H <tony.hetrick@gmail.com>
2015-07-03 16:12:02 -0600

    Created a form to submit comments

--- microscope/lib/collections/comments.js
+++ microscope/lib/collections/comments.js
@@ -1,2 +1,26 @@
 // Collection for comments on the posts
 Comments = new Mongo.Collection('comments');
+
+Meteor.methods({
+
+  // Verify data prior to adding it to the collection
+  commentInsert: function(commentAttributes) {
+    check(this.userId, String);
+    check(commentAttributes, {
+      postId: String,
+      body: String
+    });
+
+    // Get the user and data to write to the collection
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+    if (!post)
+      throw new Meteor.Error('invalid-comment', 'You must comment on a post');
+    comment = _.extend(commentAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date()
+    });
+    return Comments.insert(comment);
+  }
+});
